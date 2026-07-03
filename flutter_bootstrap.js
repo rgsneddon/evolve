2a905144c68ba2a4a93e56ab0008fe997261b5f3
rgsneddon <rgsneddon@users.noreply.github.com>
2026-07-03 11:25:24 +0100

    Evolve v2.2.6 release build (web/windows/android)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "488256651"
+    serviceWorkerVersion: "1206827984"
   }
 });
