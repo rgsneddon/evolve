039964eb0b70a32c7902950e61149a54f04a6aad
rgsneddon <rgsneddon@users.noreply.github.com>
2026-07-04 20:18:46 +0100

    Deploy web: fix cross-device PERC recipient resolution

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "950355e733bc0eba19ced6f355b8b7a2
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5d0ad25986aa320811c465d0b60dfe6b",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "81ba87775917d0c10174921e0e72b52b",
+"flutter_bootstrap.js": "b289740d3546cf0bf7b011cb8acdf56c",
 "icons/Icon-192.png": "2335bf87ddd65a5e86d14b2d4c791b4a",
 "icons/Icon-512.png": "941b8210b2e95803006ad1c285910e67",
 "icons/Icon-maskable-192.png": "638a6e5e58194982037f5170573d7b39",
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "950355e733bc0eba19ced6f355b8b7a2
 "index.html": "911d3ed117e1b7a1ac630888eb83cf43",
 "/": "911d3ed117e1b7a1ac630888eb83cf43",
 "LICENSE": "9853d95e3f26635471cebcafb1f06db9",
-"main.dart.js": "809139beb39fe92209480134b34e2101",
+"main.dart.js": "27c28c92d3dffc11709df621ad39506d",
 "manifest.json": "6abb496a5f8ecd7846edb92e8d3c3ac9",
 "README.md": "3474cc66cae4e1c77a5c3deeb00c275e",
 "version.json": "7c219311b58a322ead4d84a9c6e886ef"};
